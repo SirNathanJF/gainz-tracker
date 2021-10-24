5a8a1ca35afebfaa74357ae51ea8c7c05048a079
Nathan Flessner <nathanflessner1@gmail.com>
2021-10-23 18:22:31 -0600

    switched routes to asynchronous for apiRoutes in order to fix hero connection to database

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,56 +1,60 @@
 const router = require('express').Router();
-const db = require('../models');
+const { Workout } = require('../models');
 
 // Gets all previous workouts
-router.get('/api/workouts', (req, res) => {
-  db.Workout.find({})
-    .then(dbWorkout => {
-      console.log(dbWorkout);
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
+router.get('/api/workouts', async (req, res) => {
+    const workouts = await Workout.find();
+    console.log(workouts);
+    try {
+      res.json(workouts);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  });
 
 // Add user's new workout
-router.post('/api/workouts', ({ body }, res) => {
-  db.Workout.create(body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
+router.post('/api/workouts', async ({ body }, res) => {
+  const newWorkout = await Workout.create(body);
+  try {
+    res.status(200).json(newWorkout);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 // route for updating workout exercises by id
-router.put('/api/workouts/:id', (req, res) => {
-  db.Workout.updateOne(
-    { _id: req.params.id },
-    { $push: { exercises: req.body } }
-  )
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+router.put('/api/workouts/:id', async (req, res) => {
+  const updateWorkout = await Workout.findByIdAndUpdate(req.params.id, {
+    $push: {
+      exercises: req.body,
+    },
+  });
+  try {
+    res.status(200).json(updateWorkout);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 // Get request for last 7 workouts
-router.get('/api/workouts/range', (req, res) => {
-  // Organizes workouts by most recent first
-  db.Workout.find({})
-    .sort({ _id: -1 })
-    .limit(7)
-    .then(dbWorkout => {
-      console.log(dbWorkout);
-      res.json(dbWorkout);
-      console.log(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
+router.get('/api/workouts/range', async (req, res) => {
+  const today = new Date;
+  const weekStart = today.getDate() - today.getDay();
+  const weekEnd = weekStart + 6;
+  const rangeBegin = new Date(today.setDate(weekStart));
+  const rangeEnd = new Date(today.setDate(weekEnd));
+
+  const range = await Workout.find({
+    day: {
+      $gte: rangeBegin,
+      $lte: rangeEnd,
+    },
+  });
+
+  try {
+    res.status(200).json(range);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 module.exports = router;
