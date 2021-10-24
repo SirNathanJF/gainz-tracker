047dfe2aa17fccf6cee00811c01019a22f2e4bde
Nathan Flessner <nathanflessner1@gmail.com>
2021-10-23 18:55:37 -0600

    switched back to a different range get request route, testing for heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -35,26 +35,15 @@ router.put('/api/workouts/:id', async (req, res) => {
     res.status(400).json(error);
   }
 });
-// Get request for last 7 workouts
-router.get('/api/workouts/range', async (req, res) => {
-  const today = new Date;
-  const weekStart = today.getDate() - today.getDay();
-  const weekEnd = weekStart + 6;
-  const rangeBegin = new Date(today.setDate(weekStart));
-  const rangeEnd = new Date(today.setDate(weekEnd));
 
-  const range = await Workout.find({
-    day: {
-      $gte: rangeBegin,
-      $lte: rangeEnd,
-    },
-  });
-
-  try {
-    res.status(200).json(range);
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
+router.get('/api/workouts/range', (req, res) => {
+  Workout.find()
+    .then(dbWorkout => {
+      res.json(dbWorkout)
+    })
+    .catch(err => {
+      res.status(400).json(err)
+    })
+})
 
 module.exports = router;
